018ded112fbc64f76b76e28d12a691f3b72e430a
SCUwhite <1951816592@qq.com>
2026-01-28 10:27:23 +0800

    while循环，密码登录

--- day3for.py
+++ day3for.py
@@ -1,3 +1,18 @@
 for i in range(1,6):
     print("这是第",i,"次循环")  
+a=1
+while a<=100:
+    print(a)
+    a+=1
 
+total=0
+for i in range(1,101):
+    total+=i
+print("1到100的和是:",total)
+while True:
+    pwd=input("请输入密码:")
+    if pwd=="888888":
+        print("密码正确，欢迎使用本系统")
+        break
+    else:
+        print("密码错误，请重新输入")
